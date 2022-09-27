047c58f1ff17a08b179b314eb6e88fe290c9fb2e
jasonsin88 <jason.c.sin@gmail.com>
2022-09-27 13:01:45 -0600

    created tables for schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT,
+    INDEX manager_ind (manager_id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
