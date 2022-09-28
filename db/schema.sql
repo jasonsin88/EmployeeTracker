92104b1e8c4e59ccc9d5e9f04e3144f771bd43eb
JSin <jason.c.sin@gmail.com>
2022-09-28 08:10:20 -0600

    started dbUtil, refined schema, seeds, and package.json

--- db/schema.sql
+++ db/schema.sql
@@ -11,17 +11,16 @@ CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2) NOT NULL,
-    department_id INT,
-    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE SET NULL,
+    role_id INT NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT,
-    INDEX manager_ind (manager_id),
-    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
